cf696b1febb65192679ab93ff18735d4565701cc
marat <abylkanovm@gmail.com>
2024-11-04 17:06:14 +0500

    l02

---
+++ go.mod
@@ -0,0 +1,5 @@
+module loadtest
+
+go 1.22.2
+
+require github.com/alem-platform/ap v0.1.3 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alem-platform/ap"
+)
+
+func main() {
+	ap.PutRune('a')
+	fmt.Println(`test`)
+}
